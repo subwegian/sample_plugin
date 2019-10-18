8d09716d8f1fa89a6fc17b5ebd007a809100f1bc
subwegian <44879351+subwegian@users.noreply.github.com>
2019-10-18 03:06:47 -0700

    Add files via upload

---
+++ sample_plugin.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Sample Plugin
+Description: Let's create a WordPress plugin together!
+Version: 1.0.0
+Author: Jared & David
+Author URI: https://
+License: GPLv2 or later
+License URI: https://
+*/
+
+
+
+
